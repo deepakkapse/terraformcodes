fbda815ba141eba229a07388a4f79015666220bb
deepakk <deepak.k9845@gmail.com>
2024-11-09 18:32:01 +0530

    added vpc securitygroupid

--- main.tf
+++ main.tf
@@ -5,6 +5,7 @@ provider "aws" {
 resource "aws_instance" "example" {
   ami           =  "ami-0866a3c8686eaeeba" 
   instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.instance.id]
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
@@ -19,6 +20,7 @@ resource "aws_instance" "example" {
 
 resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
+  
   ingress {
     from_port   = 8080
     to_port     = 8080
